2b34b41b1261203c70fbea7ca7c2a401716e9ca2
swaraj Chaudhari <69323131+swarajpatil11@users.noreply.github.com>
2025-07-16 10:34:50 +0530

    Update MyReports.java

--- src/main/java/com/utilities/MyReports.java
+++ src/main/java/com/utilities/MyReports.java
@@ -10,7 +10,7 @@ public class MyReports {
 
 	public static ExtentReports getInstance() {
 
-		String path = System.getProperty("user.dir") + "/target/index.html";
+		String path = System.getProperty("user.dir") + "/test-output/index.html";
 		if (extent == null) {
 			ExtentSparkReporter sparkReporter = new ExtentSparkReporter(path);
 			sparkReporter.config().setTheme(Theme.DARK);
